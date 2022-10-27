17a52eafa8bae60bbe13947ed34e74cd95083d31
Oleg Adamov <olegadamov84@gmail.com>
2022-10-27 12:53:28 +0300

    Update HandleIncrement.jsx

--- src/components/Feedback/HandleIncrement.jsx
+++ src/components/Feedback/HandleIncrement.jsx
@@ -1,34 +1,34 @@
-export const incrementGood = () => {
-  this.setState(prevState => {
-    return {
-      good: prevState.good + 1,
-      totalFeedback: prevState.totalFeedback + 1,
-      percentageFeedback: Math.round(
-        ((this.state.good + 1) / (prevState.totalFeedback + 1)) * 100
-      ),
-    };
-  });
-};
-export const incrementNeutral = () => {
-  this.setState(prevState => {
-    return {
-      neutral: prevState.neutral + 1,
-      totalFeedback: prevState.totalFeedback + 1,
+// export const incrementGood = () => {
+//   this.setState(prevState => {
+//     return {
+//       good: prevState.good + 1,
+//       totalFeedback: prevState.totalFeedback + 1,
+//       percentageFeedback: Math.round(
+//         ((this.state.good + 1) / (prevState.totalFeedback + 1)) * 100
+//       ),
+//     };
+//   });
+// };
+// export const incrementNeutral = () => {
+//   this.setState(prevState => {
+//     return {
+//       neutral: prevState.neutral + 1,
+//       totalFeedback: prevState.totalFeedback + 1,
 
-      percentageFeedback: Math.round(
-        (this.state.good / (prevState.totalFeedback + 1)) * 100
-      ),
-    };
-  });
-};
-export const incrementBad = () => {
-  this.setState(prevState => {
-    return {
-      bad: prevState.bad + 1,
-      totalFeedback: prevState.totalFeedback + 1,
-      percentageFeedback: Math.round(
-        (this.state.good / (prevState.totalFeedback + 1)) * 100
-      ),
-    };
-  });
-};
+//       percentageFeedback: Math.round(
+//         (this.state.good / (prevState.totalFeedback + 1)) * 100
+//       ),
+//     };
+//   });
+// };
+// export const incrementBad = () => {
+//   this.setState(prevState => {
+//     return {
+//       bad: prevState.bad + 1,
+//       totalFeedback: prevState.totalFeedback + 1,
+//       percentageFeedback: Math.round(
+//         (this.state.good / (prevState.totalFeedback + 1)) * 100
+//       ),
+//     };
+//   });
+// };
